04e6f85c3025b81e5bbf04d868c4e63216a3427e
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-05-14 23:31:10 +0200

    Change from polling to webhook

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ transcriber = pipeline(model="openai/whisper-small")
 TOKEN = Credential().get_telegram_token()
 
 WEBHOOK_HOST = '134.122.70.253'
-WEBHOOK_PORT = 5000  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
